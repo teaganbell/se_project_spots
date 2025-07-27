eb79e004cae3716f1cc0703132f9376e3f3494d1
TeaganBell <teagan.bell96@gmail.com>
2025-07-27 13:50:23 -0600

    Spots Stage 5 Submission #5

--- scripts/index.js
+++ scripts/index.js
@@ -24,12 +24,14 @@ const profileDescriptionEl = document.querySelector(".profile__description");
 editProfileBtn.addEventListener("click", function () {
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
+  openModal(editProfileModal);
 });
 
 function handleEditProfileSubmit(evt) {
   evt.preventDefault();
   profileNameEl.textContent = editProfileNameInput.value;
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  closeModal(editProfileModal);
 }
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
@@ -39,21 +41,22 @@ function handleNewPostProfileSubmit(evt) {
   const imageLink = newPostImageInput.value;
   console.log(imageLink);
   const newCaption = newPostCaptionInput.value;
+  closeModal(newPostModal);
   console.log(newCaption);
 }
 
 newPostProfileForm.addEventListener("submit", handleNewPostProfileSubmit);
 
 editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 });
 
 newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
+  openModal(newPostModal);
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
+  closeModal(newPostModal);
 });
 
 function openModal(modal) {
